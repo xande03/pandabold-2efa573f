50dfab77e4bef9ceff207ff1ac618396fce0eff8
xande03 <104875527+xande03@users.noreply.github.com>
2026-03-18 21:26:06 -0300

    🚀 Lovable Deployer: troque a cor de fundo para verde escuro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "#2f4f4f",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
